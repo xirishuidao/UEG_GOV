7bbcbed22fb6c76f193bc685efaea3c827fb24bd
李天恒 <lth2302143851@outlook.com>
2024-11-15 23:42:21 +0800

    idea 测试2,开工大吉

---
+++ ueg_gov/src/loginpage/武轩负责的登录页面包下类.java
@@ -0,0 +1,4 @@
+package loginpage;
+
+public class 武轩负责的登录页面包下类 {
+}
---
+++ ueg_gov/src/startpage/startpage1.java
@@ -0,0 +1,8 @@
+package startpage;
+
+public class startpage1 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
