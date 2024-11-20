2d090ed61f197f7a0a28f6754da9ee70383632d8
李天恒 <lth2302143851@outlook.com>
2024-11-21 01:06:42 +0800

    2024/11/20
    Signed-off-by: 李天恒 <lth2302143851@outlook.com>

--- ueg_gov/src/startpage/ueg1.java
+++ ueg_gov/src/startpage/ueg1.java
@@ -1,6 +1,11 @@
 package startpage;
 
 
+
+
+import  uk.co.caprica.vlcj.binding.support.runtime.RuntimeUtil;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
 import java.awt.BorderLayout;
 import javax.swing.*;
 
@@ -11,8 +16,15 @@ class uegVidio extends JFrame {
         JPanel p1 = new JPanel();
         this.add(p1, BorderLayout.CENTER);
         //java没有内置视频软件，需要使用jmf，jlayer，vlcj等视频工具，这里使用vlc
+        //最新的vlcj因为只能github下载，所以在此学习打包jar,本项目用4.8.0
+        EmbeddedMediaPlayerComponent ueg=new EmbeddedMediaPlayerComponent();
+        ueg.mediaPlayer().media().play("ueg_gov/video/ueg_moss.mp4");
+        p1.add(ueg);
+
+
     }
 }
+
 public class ueg1 {
     private JPanel panel1;
 
@@ -20,5 +32,6 @@ public class ueg1 {
         uegVidio frame = new uegVidio();
         frame.setDefaultCloseOperation(3);
         frame.setVisible(true);
+
     }
 }
