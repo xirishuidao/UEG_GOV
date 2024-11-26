83dd2fb29f19445d2e5ecf68c694f916db73d904
李天恒 <lth2302143851@outlook.com>
2024-11-27 00:52:50 +0800

    2024/11/26 请所有人删除自己电脑上所有文件并重新加载，使用jdk1.8版本，否则请自己去下对应版本javafx库
    Signed-off-by: 李天恒 <lth2302143851@outlook.com>

--- ueg_gov/src/Main.java
+++ ueg_gov/src/Main.java
@@ -1,15 +1,52 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-public class Main {
-    public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
-        }
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        /**
+         * 首先我们创建一个Button，当我们点击Button的时候，改变Button的内容。
+         */
+
+        Button btnHello = new Button("Hello");
+        /**
+         * 设置btnHello按钮点击事件
+         * 这里使用了Java8的Lambda表达式。setOnAction的参数为EventHandler<ActionEvent> value
+         * EventHandler为一个接口，所以我们有三种方式实现EventHandler接口：
+         * 1. 创建一个内部类
+         * 2. 创建一个匿名类
+         * 3. 使用Lambda表达式（适用于函数体不大的情况）
+         */
+        btnHello.setOnAction(event->{
+            btnHello.setText("Hello World, I am JavaFX!");
+        });
+
+        /**
+         *  BorderPane是一个用于布局的Pane，BoerderPane将面板分割为上下左右中五部分。
+         *  我们可以将UI控件放置在BorderPane的上下左右和中间。
+         *  这里将将Button放置在中间。
+         */
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btnHello);
+
+        // 将pane加入到Scen中
+        Scene scene = new Scene(pane, 500, 500);
+
+        // 设置stage的scen，然后显示我们的stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+
     }
+
+    public static void main(String[] args) {
+        // JavaFX中main函数必须需要调用launch函数
+        launch(args);
+    }
+
 }
--- ueg_gov/src/startPage/ueg1.java
+++ ueg_gov/src/startPage/ueg1.java
@@ -1,12 +1,8 @@
 package startPage;
 
 
-
-
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
-
-import java.awt.BorderLayout;
 import javax.swing.*;
+import java.awt.*;
 
 class uegVidio extends JFrame {
     public uegVidio() {
@@ -16,24 +12,11 @@ class uegVidio extends JFrame {
         JPanel p1 = new JPanel();
         this.add(p1, BorderLayout.CENTER);
         p1.setVisible(true);
-        //java没有内置视频软件，需要使用jmf，jlayer，vlcj等视频工具，这里使用vlc
-        //最新的vlcj因为只能github下载，所以在此学习打包jar,本项目用4.8.0
-        EmbeddedMediaPlayerComponent ueg=new EmbeddedMediaPlayerComponent();
+        //swing不支持视频，需要使用javaFX的media
 
-        // 创建并初始化媒体播放器组件
-        p1.add(ueg, BorderLayout.CENTER);
 
-        // 在框架变得可见之后播放视频（虽然通常不是必需的，因为vlcj会在需要时初始化）
-        // 但为了安全起见，我们可以将播放代码放在一个Runnable中，并通过SwingUtilities.invokeLater()来确保它在EDT中执行
-        SwingUtilities.invokeLater(() -> {
-            if (this.isVisible()) {
-                ueg.mediaPlayer().media().play("../../../video/ueg_moss.mp4");
-            } else {
-                // 如果框架还不可见，则可以在框架变得可见时播放（例如，在一个WindowListener的windowOpened方法中）
-                // 但在这个简单的例子中，我们只需要确保在调用setVisible(true)之后播放即可
-                this.setVisible(true); // 这行其实可以省略，因为main方法中会再次调用
-            }
-        });
+
+
 
 
     }
